cdbbd5e8a1be92e9794c7bbd63a6894cf4895677
JacieChao <kathyyy@163.com>
2018-04-18 10:11:08 +0800

    add support for arm64

--- package/build-and-push.sh
+++ package/build-and-push.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+ARCH=${ARCH:?"ARCH not set"}
+SUFFIX=""
+[ "${ARCH}" != "amd64" ] && SUFFIX="${ARCH}"
 ACCT=${ACCT:-rancher}
 
-docker build -t $ACCT/rke-cert-deployer:0.1.1 .
-docker push $ACCT/rke-cert-deployer:0.1.1
+if [ "${ARCH}" != "amd64" ] .; then
+    docker build -f Dockerfile.${SUFFIX} -t $ACCT/rke-cert-deployer:0.1.1-${SUFFIX} .
+else
+    docker build -t $ACCT/rke-cert-deployer:0.1.1 .
+fi
+
+docker push $ACCT/rke-cert-deployer:0.1.1-${SUFFIX}
